7b83acc43770fa3f8df5c80d250546e99c98fbd4
n0900 <s.mueller3@gmx.at>
2025-07-17 18:26:43 +0200

    Add helper functions and prime field

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+pub mod misc;
+pub mod field;
+
+// fn secret_key_gen(params) {
+//     return todo!()
+// }
+
+// fn public_key_gen(params, sk) {
+//     return todo!()
+// }
+
+// fn enc(params,pk,m) {
+//     return todo!()
+// }
+
+// fn dec(params, sk, c) {
+//     return todo!()
+// }
+
+// fn mp_dec(params, sk, c) {
+//     return todo!()
+// }
+
 fn main() {
     println!("Hello, world!");
+    let x = field::Fp::from(1u64);
+    // bit_decomp()
 }
