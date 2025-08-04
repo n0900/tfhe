960465b3a944e45e8255c6dbc91dc55fb2690194
n0900 <s.mueller3@gmx.at>
2025-08-04 16:56:24 +0200

    RefactorMBF to struct

--- src/zo_sss/mod.rs
+++ src/zo_sss/mod.rs
@@ -12,22 +12,40 @@ pub struct Party {
 
 pub trait SecretSharingScheme {
     type ShareParameters;
-    fn share(parameter: Self::ShareParameters) -> Vec<Party>;
+    fn share(&self, parameter: Self::ShareParameters) -> Vec<Party>;
     
     type CombineParameters;
-    fn combine(parameter: Self::CombineParameters) -> Vec<Fp>;
+    fn combine(&self, parameter: Self::CombineParameters) -> Vec<Fp>;
 }
 
-pub struct MBF;
+pub struct MBF {
+    dimacs: DIMACS
+}
 
 impl SecretSharingScheme for MBF {
-    type ShareParameters = (Vec<Fp>, DIMACS);
-    fn share(parameter: Self::ShareParameters) -> Vec<Party> {
-        mbf_share(parameter.0, &parameter.1)
+    type ShareParameters = Vec<Fp>;
+    fn share(&self, parameter: Self::ShareParameters) -> Vec<Party> {
+        mbf_share(parameter, &self.dimacs)
     }
 
-    type CombineParameters = (Vec<Party>, bool, DIMACS);
-    fn combine(parameter: Self::CombineParameters) -> Vec<Fp> {
-        mbf_combine(parameter.0, parameter.1, &parameter.2)
+    type CombineParameters = (Vec<Party>, bool);
+    fn combine(&self, parameter: Self::CombineParameters) -> Vec<Fp> {
+        mbf_combine(parameter.0, parameter.1, &self.dimacs)
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::{field::P, misc::rnd_fp_vec, zo_sss::{dimacs::{self, DIMACS, DIMACS_2_OF_3_SCHEME}, SecretSharingScheme, MBF}};
+
+    #[test]
+    fn mbf_struct_test() {
+        let secrets = rnd_fp_vec(5, 0, P-1);
+        let copy_secrets = secrets.clone();
+        let mbf = MBF { dimacs: DIMACS::parse(DIMACS_2_OF_3_SCHEME) };
+        let parties = mbf.share(secrets);
+        let result = mbf.combine((parties, false));
+        assert_eq!(copy_secrets, result);
     }
 }
