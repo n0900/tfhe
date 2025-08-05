e34994bcb1fe47fe818449a315ab63ad18c59905
n0900 <s.mueller3@gmx.at>
2025-08-05 19:59:23 +0200

    Add special Shamir struct

--- src/zo_sss/mod.rs
+++ src/zo_sss/mod.rs
@@ -15,6 +15,27 @@ pub trait SecretSharingScheme {
     fn combine(&self, parties: Vec<Party>, is_minimal: bool) -> Vec<Fp>;
 }
 
+/// It is the users responsibility to use a correct dimacs!
+/// Only checks for k-out-of-n
+pub struct Shamir {
+    _n: u8,
+    k: u8,
+    dimacs: DIMACS
+}
+
+impl SecretSharingScheme for Shamir {
+    fn share(&self, secrets: Vec<Fp>) -> Vec<Party> {
+        mbf_share(secrets, &self.dimacs)
+    }
+
+    fn combine(&self, parties: Vec<Party>, _is_minimal: bool) -> Vec<Fp> {
+        let subset: Vec<Party> = if parties.len() >= (self.k as usize) {
+            parties.iter().take(self.k as usize).cloned().collect()
+        } else { panic!("Invalid party size") };
+        mbf_combine(subset, true, &self.dimacs)
+    }
+}
+
 pub struct MBF {
     dimacs: DIMACS
 }
@@ -31,15 +52,25 @@ impl SecretSharingScheme for MBF {
 
 #[cfg(test)]
 mod tests {
-    use crate::{field::P, misc::rnd_fp_vec, zo_sss::{dimacs::{DIMACS, DIMACS_2_OF_3_SCHEME}, SecretSharingScheme, MBF}};
+    use crate::{field::P, misc::rnd_fp_vec, zo_sss::{dimacs::{DIMACS, DIMACS_2_OF_3_SCHEME, DIMACS_AB_OR_CD}, SecretSharingScheme, Shamir, MBF}};
+
+    #[test]
+    fn shamir_struct_test() {
+        let shamir = Shamir { _n:3, k:2, dimacs: DIMACS::parse(DIMACS_2_OF_3_SCHEME) };
+        sss_test(shamir);
+    }
 
     #[test]
     fn mbf_struct_test() {
+        let mbf = MBF { dimacs: DIMACS::parse(DIMACS_AB_OR_CD) };
+        sss_test(mbf);
+    }
+
+    fn sss_test<T: SecretSharingScheme>(scheme: T) {
         let secrets = rnd_fp_vec(5, 0, P-1);
         let copy_secrets = secrets.clone();
-        let mbf = MBF { dimacs: DIMACS::parse(DIMACS_2_OF_3_SCHEME) };
-        let parties = mbf.share(secrets);
-        let result = mbf.combine(parties, false);
+        let parties = scheme.share(secrets);
+        let result = scheme.combine(parties, false);
         assert_eq!(copy_secrets, result);
     }
 }
