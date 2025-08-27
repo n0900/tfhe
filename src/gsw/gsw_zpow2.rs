ee3d3c77fdb7ab844b345377c2f36891294c0c07
n0900 <s.mueller3@gmx.at>
2025-08-27 17:39:54 +0200

    Prepare Zpow2 gsw implementation

---
+++ src/gsw/gsw_zpow2.rs
@@ -0,0 +1,8 @@
+// use crate::{error_sampling::ErrorSampling, gsw::{FheScheme, GSW}};
+
+
+// impl<R: RingElement, T: ErrorSampling> FheScheme for GSW<T> {
+//     type SecretKey = GswSk;
+//     type PublicKey = GswPk;
+//     type Ciphertext = Vec<Vec<Fp>>;
+// }
